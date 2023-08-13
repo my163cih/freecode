d0c78ac60601e7398f8ea3454fd36c6f20eca9cb
Yao M <my163cih@gmail.com>
2023-08-13 02:08:14 -0700

    NavBar

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,20 @@
+import Navbar from '@/components/navbar/Navbar'
+import React from 'react'
+
+type AuthPageProps = {
+
+}
+
+const AuthPage: React.FC<AuthPageProps> = () => {
+  return (
+    <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+      <div className='max-w-7xl mx-auto'>
+        <Navbar />
+      </div>
+    </div>
+  )
+}
+
+
+
+export default AuthPage
