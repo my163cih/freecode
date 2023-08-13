d3bd4ffaf00c7fd17bf8fba3697cd8dfcb871935
Yao M <my163cih@gmail.com>
2023-08-13 02:56:22 -0700

    auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/navbar/Navbar'
+import Image from 'next/image'
 import React from 'react'
 
 type AuthPageProps = {
@@ -7,9 +8,13 @@ type AuthPageProps = {
 
 const AuthPage: React.FC<AuthPageProps> = () => {
   return (
-    <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+    <div className='bg-gradient-to-b from-black to-gray-200 h-screen relative'>
       <div className='max-w-7xl mx-auto'>
         <Navbar />
+        <div className='flex flex-col items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none '>
+          <Image src='/hero2.png' alt='' width={500} height={250}/>
+          <Image src='/tech_companies.png' alt='' width={700} height={400}/>
+        </div>
       </div>
     </div>
   )
